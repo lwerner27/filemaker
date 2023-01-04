4b63395b692bb13a66cd7ac6b977051e99993029
Lucas Werner <lwerner27@gmail.com>
2023-01-03 20:36:36 -0600

    Added comments to the code

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ const app = express()
 PORT = 3000 || process.env.PORT
 
 app.get('/', (req, res) => {
+
+    // Creates a very basic JSON file wth very basic data inside
     fs.writeFile('data.json', JSON.stringify({"Hello":"World"}), (err) => {
         if (err) throw err
         console.log("Saved!")
     })
 
+    // Sends the response to the request using res.download 
+    // This makes it so the file is downloaded by the device making the request
     res.download("data.json", "YourData.json", (err) => {
         if (err) throw err
+
+        // This deletes the file after it is downloaded
         fs.unlink("data.json", () => {
             console.log("File Deleted.")
         })
